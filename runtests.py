86a4eb6d11260b9b89dfcf2ba82082fdce394cf9
Victor Uriarte <victor.m.uriarte@intel.com>
2016-09-17 21:18:45 -0700

    simplify test

--- runtests.py
+++ runtests.py
@@ -1,145 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import optparse
-import os
-import shutil
 import sys
 import unittest
 
-
-def collect():
-    import tests
-    runtests(tests, 1)
+import tests_core
 
 
-def runtests(suite, verbosity):
+def runtests(suite, verbosity=1):
     results = unittest.TextTestRunner(verbosity=verbosity).run(suite)
     return results.failures, results.errors
 
 
-def get_option_parser():
-    parser = optparse.OptionParser()
-    basic = optparse.OptionGroup(parser, 'Basic test options')
-    basic.add_option(
-        '-e',
-        '--engine',
-        dest='engine',
-        help=('Database engine to test, one of '
-              '[sqlite, postgres, mysql, apsw, sqlcipher, berkeleydb]'))
-    basic.add_option('-v', '--verbosity', dest='verbosity', default=1,
-                     type='int', help='Verbosity of output')
-
-    suite = optparse.OptionGroup(parser, 'Simple test suite options')
-    suite.add_option('-a', '--all', dest='all', default=False,
-                     action='store_true',
-                     help='Run all tests, including extras')
-    suite.add_option('-x', '--extra', dest='extra', default=False,
-                     action='store_true', help='Run only extras tests')
-
-    cases = optparse.OptionGroup(parser, 'Individual test module options')
-    cases.add_option('--apsw', dest='apsw', default=False, action='store_true',
-                     help='apsw tests (requires apsw)')
-    cases.add_option('--berkeleydb', dest='berkeleydb', default=False,
-                     action='store_true',
-                     help='berkeleydb tests (requires pysqlite compiled against berkeleydb)')
-    cases.add_option('--csv', dest='csv', default=False, action='store_true',
-                     help='csv tests')
-    cases.add_option('--dataset', dest='dataset', default=False,
-                     action='store_true', help='dataset tests')
-    cases.add_option('--db-url', dest='db_url', default=False,
-                     action='store_true', help='db url tests')
-    cases.add_option('--djpeewee', dest='djpeewee', default=False,
-                     action='store_true', help='djpeewee tests')
-    cases.add_option('--fields', dest='fields', default=False,
-                     action='store_true', help='extra field tests')
-    cases.add_option('--flask', dest='flask', default=False,
-                     action='store_true', help='flask utils tests')
-    cases.add_option('--gfk', dest='gfk', default=False, action='store_true',
-                     help='gfk tests')
-    cases.add_option('--hybrid', dest='hybrid', default=False,
-                     action='store_true', help='hybrid property/method tests')
-    cases.add_option('--kv', dest='kv', default=False, action='store_true',
-                     help='key/value store tests')
-    cases.add_option('--manytomany', dest='manytomany', default=False,
-                     action='store_true', help='manytomany field tests')
-    cases.add_option('--migrations', dest='migrations', default=False,
-                     action='store_true',
-                     help='migration helper tests (requires psycopg2)')
-    cases.add_option('--pool', dest='pool', default=False, action='store_true',
-                     help='connection pool tests')
-    cases.add_option('--postgres-ext', dest='postgres_ext', default=False,
-                     action='store_true',
-                     help='postgres_ext tests (requires psycopg2)')
-    cases.add_option('--pwiz', dest='pwiz', default=False, action='store_true',
-                     help='pwiz, model code generator')
-    cases.add_option('--read-slave', dest='read_slave', default=False,
-                     action='store_true', help='read_slave tests')
-    cases.add_option('--reflection', dest='reflection', default=False,
-                     action='store_true',
-                     help='reflection schema introspector')
-    cases.add_option('--signals', dest='signals', default=False,
-                     action='store_true', help='signals tests')
-    cases.add_option('--shortcuts', dest='shortcuts', default=False,
-                     action='store_true', help='shortcuts tests')
-    cases.add_option('--speedups', dest='speedups', default=False,
-                     action='store_true', help='speedups c extension tests')
-    cases.add_option('--sqlcipher-ext', dest='sqlcipher', default=False,
-                     action='store_true',
-                     help='sqlcipher_ext tests (requires pysqlcipher)')
-    cases.add_option('--sqlite-c-ext', dest='sqlite_c', default=False,
-                     action='store_true', help='sqlite c extension tests')
-    cases.add_option('--sqlite-ext', dest='sqlite_ext', default=False,
-                     action='store_true', help='sqlite_ext tests')
-    cases.add_option('--sqlite-udf', dest='sqlite_udf', default=False,
-                     action='store_true', help='sqlite_udf tests')
-    cases.add_option('--test-utils', dest='test_utils', default=False,
-                     action='store_true', help='test_utils tests')
-
-    parser.add_option_group(basic)
-    parser.add_option_group(suite)
-    parser.add_option_group(cases)
-    return parser
-
-
-def collect_modules(options):
+def collect_modules():
     modules = []
-    import tests_core
     modules.insert(0, tests_core)
     return modules
 
 
 if __name__ == '__main__':
-    parser = get_option_parser()
-    options, args = parser.parse_args()
-
-    if options.engine:
-        os.environ['PEEWEE_TEST_BACKEND'] = options.engine
-    os.environ['PEEWEE_TEST_VERBOSITY'] = str(options.verbosity)
-
-    from peewee import print_
 
     suite = unittest.TestSuite()
-    for module in collect_modules(options):
-        print_('Adding tests for "%s"' % module.__name__)
+    for module in collect_modules():
         module_suite = unittest.TestLoader().loadTestsFromModule(module)
         suite.addTest(module_suite)
 
-    failures, errors = runtests(suite, options.verbosity)
-
-    files_to_delete = [
-        'peewee_test.db',
-        'peewee_test',
-        'tmp.db',
-        'peewee_test.bdb.db',
-        'peewee_test.cipher.db']
-    paths_to_delete = ['peewee_test.bdb.db-journal']
-    for filename in files_to_delete:
-        if os.path.exists(filename):
-            os.unlink(filename)
-    for path in paths_to_delete:
-        if os.path.exists(path):
-            shutil.rmtree(path)
+    failures, errors = runtests(suite)
 
     if errors:
         sys.exit(2)
