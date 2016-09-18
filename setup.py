f3cddccc30669fd09f275840608f6d23ce7fea88
Victor Uriarte <victor.m.uriarte@intel.com>
2016-09-17 23:11:48 -0700

    prep to change from module to package

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='peewee',
     version='2.8.3',
+    license='MIT',
     author='Charles Leifer',
     author_email='coleifer@gmail.com',
-    py_modules=['peewee'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
 )
