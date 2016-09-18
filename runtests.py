a45049d314c1f9fedfb1703a0196ae63de3ba966
Victor Uriarte <victor.m.uriarte@intel.com>
2016-09-17 20:47:00 -0700

    Add headers

--- runtests.py
+++ runtests.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import optparse
 import os
 import shutil
--- tests_core.py
+++ tests_core.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 
 Aggregate all the test modules and run from the command-line. For information
